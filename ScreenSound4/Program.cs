1785e88d9a52b59b6cc86a8be4801a8dfaf9ccce
GuilhermeSerafim <guilerstudies@gmail.com>
2024-04-18 13:25:35 -0300

    Criando cliente e extraindo dados da api de musicas

--- ScreenSound4/Program.cs
+++ ScreenSound4/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿// Esse using tem um contexto diferente - Gerenciar recurso
+using (HttpClient client = new())
+{
+    // Recurso
+    string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json"); // Utilizamos o async, pois não sabemos quantas músicas tem
+    Console.WriteLine(resposta); // Dados da api
+
+    // Liberar Recurso
+}
