0c8af0694a4a252722e952fef348561640ed7574
GuilhermeSerafim <guilerstudies@gmail.com>
2024-04-18 16:27:47 -0300

    Criando função estatica de retornar todos os albuns de musicas, cujo valor seja diferente do outro através de HashSet

--- ScreenSound4/Model/Musica.cs
+++ ScreenSound4/Model/Musica.cs
@@ -24,4 +24,25 @@ internal class Musica
         Console.WriteLine($"Genêro musical: {Genero}");
         Console.WriteLine($"Duracao: {Duracao / 1000}");
     }
+    public static void ExibirTodosGenerosMusicais(List<Musica> musicas)
+    {
+        // Extraindo musicas para genero unico
+        // Criar um HashSet para armazenar gêneros musicais únicos
+        HashSet<string> generosUnicos = new HashSet<string>();
+        // Percorrer todas as musicas
+        foreach(Musica musica in musicas)
+        {
+
+            // Antes de adicionar a musica ao genero unico, preciso realizar alguns procedimentos
+            generosUnicos.Add(musica.Genero!);
+        }
+        // Após abstrair os generos vou exibir no console, ou retornar dependendo da proposta
+        Console.WriteLine("Todos os generos musicais: ");
+        // Extraindo musicas para albuns
+        foreach (var genero in generosUnicos)
+        {
+            Console.WriteLine(genero);
+            Console.WriteLine();
+        }
+    }
 }
--- ScreenSound4/Program.cs
+++ ScreenSound4/Program.cs
@@ -8,9 +8,10 @@ using (HttpClient client = new())
     {
         // Utilizamos o async, pois não sabemos quantas músicas tem
         string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
-        // Desserialização (metodo statico)
+        // Desserialização (metodo estatico)
         List<Musica> musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!; // Indicando que não é nulo !
-        musicas[1998].ExibirInfoMusica();
+        //musicas[1998].ExibirInfoMusica();
+        Musica.ExibirTodosGenerosMusicais(musicas);
     }
     catch (Exception ex)
     {
@@ -21,3 +22,6 @@ using (HttpClient client = new())
 }
 // Embora o código fornecido não inclua uma operação explícita de serialização, a função JsonSerializer pode ser usada tanto
 // para serializar (converter objetos em uma representação JSON) quanto para desserializar (converter JSON em objetos).
+
+// Desafio 1 - Exibir todos os generos de musicas da lista
+
