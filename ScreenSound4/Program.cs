f32cda1bf4154c1942b0487eff750e319474d6e2
GuilhermeSerafim <guilerstudies@gmail.com>
2024-04-18 13:28:43 -0300

    Aprendendo capturar exceçoes

--- ScreenSound4/Program.cs
+++ ScreenSound4/Program.cs
@@ -2,8 +2,16 @@
 using (HttpClient client = new())
 {
     // Recurso
-    string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json"); // Utilizamos o async, pois não sabemos quantas músicas tem
-    Console.WriteLine(resposta); // Dados da api
+    try
+    {
+
+        string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.jso"); // Utilizamos o async, pois não sabemos quantas músicas tem
+        Console.WriteLine(resposta); // Dados da api
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Temos um problema: " + ex.Message);
+    }
 
     // Liberar Recurso
 }
